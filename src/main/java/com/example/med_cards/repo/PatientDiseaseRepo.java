6da817b171d960ded109c4ad94c9db89413f6571
Gregorii00 <zhevner.00@yandex.ru>
2024-07-31 20:09:46 +0400

    Second med card db with migrations

--- src/main/java/com/example/med_cards/repo/PatientDiseaseRepo.java
+++ src/main/java/com/example/med_cards/repo/PatientDiseaseRepo.java
@@ -1,11 +1,9 @@
 package com.example.med_cards.repo;
 
-import com.example.med_cards.model.Disease;
 import com.example.med_cards.model.PatientDisease;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
